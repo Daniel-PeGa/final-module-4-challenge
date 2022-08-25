3c738f7af6f601513d32e030cd39b84227953ce1
Daniel Perez Garnica <danielperezgarnic@gmail.com>
2022-08-25 09:54:24 -0500

    ya tenemos la warehouse llena y vamos pa delante con otro

---
+++ scripts/questions.js
@@ -0,0 +1,52 @@
+var wareHouse = [
+    {
+        question: "What's the less-than-or-equal-to symbol in programing?",
+        selection: ["<", ">", "<=", ">="],
+        answer: "<=",
+    },
+    {
+        question: "What makes a string??",
+        selection: ["text", "numbers", "symbols", "formulas"],
+        answer: "text",
+    },
+    {
+        question: "From which number does the amount of elements in an array begin??",
+        selection: ["0", "1", "-1", "-0"],
+        answer: "0",
+    },
+    {
+        question: "How many possible values does a boolean have??",
+        selection: ["0","2", "10", "infinite"],
+        answer: "2",
+    },
+    {
+        question: "This means it's equal in form and value:",
+        selection: ["=", "==", "===", "!=="],
+        answer: "===",
+    },
+    {
+        question: "Who invented the internet??",
+        selection: ["AlGore", "Albert Einstein", "Antonio Meucci", "Vinton Cerf and Bob Kahn"],
+        answer: "Vinton Cerf and Bob Kahn",
+    },
+    {
+        question: "Is there a difference between Java and JavaScript",
+        selection: ["Yes.", "nah, same thing", "idk, is there?", "p e r h a p s"],
+        answer: "Yes.",
+    },
+    {
+        question: "What does CSS mean?",
+        selection: ["Confidential Secret Society", "Counting Style Sheets", "Cascading Style Sheets", "Camp Sweet Summer"],
+        answer: "Cascading Style Sheets",
+    },
+    {
+        question: "What's my favourite color?",
+        selection: ["White", "Red", "Green", "Black"],
+        answer: "Red",
+    },
+    {
+        question: "What does <hr> in HTML do?",
+        selection: ["Thematic break", "Tax break", "Gives you the time", "Measure in hours"],
+        answer: "Thematic break",
+    },
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 var startQuiz = document.querySelector("#startButton");
 var highButton = document.querySelector("#highscores");
 var timeLeft = document.querySelector(".timeLeft");
+
 var mainGame = document.querySelector("#mainGame");
 var question = document.querySelector("#question");
 var choiceA = document.querySelector("#choiceA");
@@ -10,3 +11,31 @@ var choiceD = document.querySelector("#choiceD");
 var answer = document.querySelector('#answer');
 var feedback = document.querySelector('#responseRe');
 var card = document.querySelector('#questionHeader');
+
+var inputForm = document.querySelector('#inputForm');
+var scores = document.querySelector('#scores');
+var scoreBtn = document.querySelector('#scoreBtn');
+var initials = document.querySelector('#namePlay');
+var submitBtn = document.querySelector('#submitBtn');
+var back = document.querySelector('#backButton');
+
+var remTime = wareHouse.length * 15;
+var q = 0;
+var s = 0;
+var score = 0;
+var scoreTab = [];
+var timeInterval;
+
+getScore();
+
+function timer() {
+    timeInterval = setInterval(function () {
+        remLeft--;
+        timerDisplay.textContent = "You have now " + remTime + " left!!";
+        if (remTime == 0 || q>= wareHouse.length) {
+            clearInterval(timeInterval);
+            gameOver();
+        }
+    }, 1000);
+}
+
